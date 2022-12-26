056ff90fad5400574eac09d1ab772489cf05a4d5
VJTIWARI9939 <vijaytiwari9939@gmail.com>
2022-12-26 15:51:24 +0530

    modified vijay.java

--- vijay.java
+++ vijay.java
@@ -1,5 +1,6 @@
 class vijay  {
      public static void main(String[]args){
       System.out.println("you can do this full playlist");
+      System.out.println("you can do this full playlist");
             }
   }
